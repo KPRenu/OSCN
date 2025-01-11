02584840004f6b739a3ae112f573db62a54d0987
KPRenu <130354166+KPRenu@users.noreply.github.com>
2025-01-11 08:22:33 +0530

    Update 11a.c

--- 11a.c
+++ 11a.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
-struct fileTable
-{
+
+struct fileTable {
     char name[20];
-    int sb, nob;
+    int sb, nob; 
 } ft[30];
-void main()
-{
+
+int main() {
     int i, j, n;
     char s[20];
   
-    printf("Enter no of files :");
+    printf("Enter number of files: ");
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
-    {
-        printf("\nEnter file name %d :", i + 1);
+
+    for (i = 0; i < n; i++) {
+        printf("\nEnter file name %d: ", i + 1);
         scanf("%s", ft[i].name);
-        printf("Enter starting block of file %d :", i + 1);
+        printf("Enter starting block of file %d: ", i + 1);
         scanf("%d", &ft[i].sb);
-        printf("Enter no of blocks in file %d :", i + 1);
+        printf("Enter number of blocks in file %d: ", i + 1);
         scanf("%d", &ft[i].nob);
     }
-    printf("\nEnter the file name to be searched -- ");
+
+    printf("\nEnter the file name to be searched: ");
     scanf("%s", s);
-    for (i = 0; i < n; i++)
-        if (strcmp(s, ft[i].name) == 0)
-            break;
-    if (i == n)
-        printf("\nFile Not Found");
-    else
-    {
-        printf("\nFILE NAME START BLOCK NO OF BLOCKS BLOCKS OCCUPIED\n");
-        printf("\n%s\t\t%d\t\t%d\t", ft[i].name, ft[i].sb, ft[i].nob);
-        for (j = 0; j < ft[i].nob; j++)
-            printf("%d, ", ft[i].sb + j);
+
+    for (i = 0; i < n; i++) {
+        if (strcmp(s, ft[i].name) == 0) {
+            printf("\nFILE NAME\tSTART BLOCK\tNO OF BLOCKS\tBLOCKS OCCUPIED\n");
+            printf("%s\t\t%d\t\t%d\t\t", ft[i].name, ft[i].sb, ft[i].nob);
+            for (j = 0; j < ft[i].nob; j++) {
+                if (j == ft[i].nob - 1)
+                    printf("%d", ft[i].sb + j);
+                else
+                    printf("%d, ", ft[i].sb + j);
+            }
+            printf("\n");
+            return 0; 
+        }
     }
+    
+    printf("\nFile Not Found\n");
+    return 0;
 }
